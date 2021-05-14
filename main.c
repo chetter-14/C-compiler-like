f21bf2785199c1f4114cc132247a40cf86c8d9c5
ArtemLeshch <50210899+ArtemLeshch@users.noreply.github.com>
2021-05-14 13:56:44 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,40 +1,84 @@
 #include <stdbool.h>
 #include <stdio.h>
+#define ERRORLINESAMOUNT 10
 
 
 int main()
 {
 	char c;
-	bool isCorrect, toBeTerminated = false;
-	int nl = 1;
+	bool isEverythingFine = true, parToBeTerminated = false;
+	int bracesToBeTerminated = 0;
+	int nl = 1, i = 0;					// i - to access errorlines[] array 
+	int errorLines[ERRORLINESAMOUNT]; 	// array of integers representing lines where a curly brackets error occured
+	
 	
 	FILE* f = fopen("test.c", "r");
 	
 	while ((c = getc(f)) != EOF)
 	{
-		if (c == '(')
+		switch(c)
 		{
-			toBeTerminated = true;
-			while ((c = getc(f)) != '\n')
-			{
-				if (c == ')')
-				{
-					toBeTerminated = false;
-					break;
-				}
-			}
-		}
-		if (toBeTerminated)
-		{
-			printf("The parentheses should be closed on line %d\n", nl);
+		case '(':
+			parToBeTerminated = true;
 			break;
-		}
-		if (c == '\n')
+			
+		case ')':
+			if (parToBeTerminated)
+			{
+				parToBeTerminated = false;
+				break;
+			}
+			else 
+			{
+				printf("There isn't open round bracket on line %d\n", nl);
+				isEverythingFine = false;
+				break;
+			}
+
+		case '\n':
+			if (parToBeTerminated)
+			{
+				printf("Round brackets should be closed on line %d\n", nl);
+				isEverythingFine = false;
+				parToBeTerminated = false;
+			}
 			nl++;
+			break;
+		
+		case '{':
+			bracesToBeTerminated++;
+			errorLines[i] = nl;
+			i++;
+			break;
+		
+		case '}':
+			if (bracesToBeTerminated)
+			{
+				bracesToBeTerminated--;
+				i--;
+				break;
+			}
+			else
+			{
+				printf("No matching open curly bracket on line %d\n", nl);
+				isEverythingFine = false;
+				break;
+			}
+			
+		default:
+			;
+		}
 	}
 	
-	if (!toBeTerminated)
-		printf("Everything went fine!");
+	if (bracesToBeTerminated)
+	{
+		isEverythingFine = false;
+		for (int j = 0; j < i; j++)
+			printf("No matching close curly bracket on line %d\n", errorLines[j]);
+	}
+	
+	if (isEverythingFine)
+		printf("Everything went fine!\n");
 		
 	fclose(f);
 	return 0;
